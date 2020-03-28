29faf32c95ea32285ea97121fd8ccf0c24aa27b8
Margotte <margo.tinawi@gmail.com>
2020-03-28 14:56:48 -0400

    Slider instead of form

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+var rollsSlider = document.getElementById("rolls");
+var rollsOutput = document.getElementById("rollsValue");
+rollsOutput.innerHTML = rollsSlider.value;
+
+rollsSlider.oninput = function() {
+  rollsOutput.innerHTML = this.value;
+  calculateRolls();
+}
+
+var daysSlider = document.getElementById("days");
+var daysOutput = document.getElementById("daysValue");
+daysOutput.innerHTML = daysSlider.value;
+
+daysSlider.oninput = function() {
+  daysOutput.innerHTML = this.value;
+  calculateRolls();
+}
+
 function calculateRolls() {
-  const rolls = event.target[0].value;
-  const quarantineDays = event.target[1].value;
+
+  const rolls = rollsSlider.value;
+  const quarantineDays = daysSlider.value;
+
+  console.log(rolls);
+  console.log(quarantineDays);
 
   const paperDays = rolls * 5; // A roll lasts 5 days
 
   const ratio = paperDays / quarantineDays * 100 ;
-  console.log(ratio);
 
   const ratioDiv = document.getElementById('ratio');
 
@@ -13,12 +34,7 @@ function calculateRolls() {
     const emoji = "😄";
     ratioDiv.innerText = `${Math.round(ratio)}% ${emoji}`;
   } else {
-    const emoji = "💩";
+    const emoji = "💩💩";
     ratioDiv.innerText = `${Math.round(ratio)}%  ${emoji}`;
   }
-
-  event.preventDefault();
 };
-
-const form = document.getElementById('form');
-form.addEventListener('submit', calculateRolls);
